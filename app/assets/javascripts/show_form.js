4810bb7461791092fe03918bf2424c2ae5223dfe
Reid Shea <reidshea3@gmail.com>
2022-06-22 13:13:30 -0400

    fixed button position

--- app/assets/javascripts/show_form.js
+++ app/assets/javascripts/show_form.js
@@ -4,43 +4,41 @@ if (infrCardAdd !== null) {
   infrCardAdd.addEventListener("click", showHide);
 }
 
-
-
 function showHide(event) {
-    var span = document.getElementsByClassName("close")[0];
-    var form = document.getElementById("show-form");
-    var button = document.getElementById("form_button");
-    var mask = document.getElementById("page-mask");
-    button.classList.add("disabled");
-    mask.style.display = "block";
-    form.style.display = "block";
-    span.onclick = function() {
-      form.style.display = "none";
-      mask.style.display = "none";
-      button.classList.remove("disabled");
-    };
-    // Do not uncomment - we will use it later!!!
-    // window.onclick = function(event) {
-    //   if (event.target == form) {
-    //     form.style.display = "none";
-    //     button.classList.add("enabled");
-    //   }
-    // };
+  var span = document.getElementsByClassName("close")[0];
+  var form = document.getElementById("show-form");
+  var button = document.getElementById("form_button");
+  var mask = document.getElementById("page-mask");
+  button.classList.add("disabled");
+  mask.style.display = "block";
+  form.style.display = "block";
+  span.onclick = function () {
+    form.style.display = "none";
+    mask.style.display = "none";
+    button.classList.remove("disabled");
+  };
+  // Do not uncomment - we will use it later!!!
+  // window.onclick = function(event) {
+  //   if (event.target == form) {
+  //     form.style.display = "none";
+  //     button.classList.add("enabled");
+  //   }
+  // };
 }
 
 // TODO: REFACTOR THIS CODE TO BE MORE DRY
 
 function showHideEdit(event) {
-    var span = document.getElementsByClassName("close-edit")[0];
-    var form = document.getElementById("show-form-edit");
-    var button = document.getElementById("form_button_edit");
-    var mask = document.getElementById("page-mask");
-    button.classList.add("disabled");
-    form.style.display = "block";
-    mask.style.display = "block";
-    span.onclick = function() {
-      form.style.display = "none";
-      button.classList.remove("disabled");
-      mask.style.display = "none";
-    };
+  var span = document.getElementsByClassName("close-edit")[0];
+  var form = document.getElementById("show-form-edit");
+  var button = document.getElementById("form_button_edit");
+  var mask = document.getElementById("page-mask");
+  button.classList.add("disabled");
+  form.style.display = "block";
+  mask.style.display = "block";
+  span.onclick = function () {
+    form.style.display = "none";
+    button.classList.remove("disabled");
+    mask.style.display = "none";
+  };
 }
--- app/assets/javascripts/snapshot_page.js
+++ app/assets/javascripts/snapshot_page.js
@@ -7,45 +7,44 @@ if (btnPrintSnapshot !== null) {
   pathArray = window.location.pathname.split("/");
   let snapshotId = pathArray[pathArray.length - 1];
 
-
   btnPrintSnapshot.addEventListener("click", (event) => {
     window.print();
     return false;
   });
 
   const leftAjax = (event) => {
-    let id = document.getElementById("img-snapshots").dataset.arrayid
+    let id = document.getElementById("img-snapshots").dataset.arrayid;
     Rails.ajax({
       url: window.location.pathname + "/nextsnapshot",
       type: "get",
       data: `image=previous&snapshot_id=${id}`,
-      success: (data) => { console.log(data);},
-      error: (data) => {}
+      success: (data) => {
+        console.log(data);
+      },
+      error: (data) => {},
     });
-  }
+  };
 
   const rightAjax = (event) => {
-    let id = document.getElementById("img-snapshots").dataset.arrayid
+    let id = document.getElementById("img-snapshots").dataset.arrayid;
     Rails.ajax({
       url: window.location.pathname + "/nextsnapshot",
       type: "get",
       data: `image=next&snapshot_id=${id}`,
-      success: (data) => { },
-      error: (data) => {}
+      success: (data) => {},
+      error: (data) => {},
     });
-  }
+  };
 
-  document.addEventListener('keydown', (event) =>  {
-    if (event.code === 'ArrowLeft') {
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
       leftAjax(event);
     }
-    if (event.code === 'ArrowRight') {
+    if (event.code === "ArrowRight") {
       rightAjax(event);
     }
   });
 
-  arrowRight.addEventListener('click', rightAjax);
-  arrowLeft.addEventListener('click', leftAjax);
+  arrowRight.addEventListener("click", rightAjax);
+  arrowLeft.addEventListener("click", leftAjax);
 }
-
-
