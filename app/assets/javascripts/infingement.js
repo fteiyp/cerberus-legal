5c55ce9399939d48a16bdf48ad5c959443c73f22
Reid Shea <reidshea3@gmail.com>
2022-06-28 14:24:47 -0400

    fixed background svg, sidekiq is working

--- app/assets/javascripts/infingement.js
+++ app/assets/javascripts/infingement.js
@@ -1,9 +1,11 @@
-const selectTimer =  document.getElementById("select-timer");
+const selectTimer = document.getElementById("select-timer");
 const playButton = document.getElementById("play-button");
 const snapshotButton = document.getElementById("snapshot-button");
 const exportButton = document.getElementById("export-button");
 const deleteButton = document.getElementById("delete-button");
-const csfrToken = document.head.querySelector("[name~=csrf-token][content]").content;
+const csfrToken = document.head.querySelector(
+  "[name~=csrf-token][content]"
+).content;
 
 // Select items
 const selectMenu = document.querySelector(".select-menu-infringement");
@@ -18,10 +20,6 @@ const invertSelectionButton = document.getElementById("invert-button");
 let numberOfSelected = 0;
 
 if (selectTimer != null) {
-
-
-
-
   // Select functions and actions
 
   const selectedParametersString = () => {
@@ -32,29 +30,29 @@ if (selectTimer != null) {
       }
     }
     return selectedString.substring(1);
-  }
+  };
 
   const updateSelectedCount = () => {
     selectedText[0].innerText = `${numberOfSelected} Selected`;
-  }
+  };
 
   const updateExportAndDeleteButton = () => {
     if (numberOfSelected === 0 || numberOfSelected === checkBoxes.length) {
       exportButton.innerText = `Export All`;
       deleteButton.innerText = `Delete All`;
     } else {
-      exportButton.innerText = `Export (${numberOfSelected})`
-      deleteButton.innerText = `Delete (${numberOfSelected})`
+      exportButton.innerText = `Export (${numberOfSelected})`;
+      deleteButton.innerText = `Delete (${numberOfSelected})`;
     }
-  }
+  };
 
   const showSelectMenu = () => {
     selectMenu.style.visibility = "visible";
-  }
+  };
 
   const hideSelectMenu = () => {
     selectMenu.style.visibility = null;
-  }
+  };
 
   const showCheckCards = () => {
     for (let i = 0; i < checkCards.length; i += 1) {
@@ -62,10 +60,10 @@ if (selectTimer != null) {
       cards[i].classList.add("card-trip-noclick");
       checkCards[i].style.visibility = "visible";
     }
-  }
+  };
 
   const hideCheckCards = () => {
-    let checkFlag = false
+    let checkFlag = false;
     for (let i = 0; i < checkBoxes.length; i += 1) {
       if (checkBoxes[i].dataset.checked === "true") {
         checkFlag = true;
@@ -80,17 +78,17 @@ if (selectTimer != null) {
       }
       hideSelectMenu();
     }
-  }
+  };
 
   const cardClickOn = (event) => {
     for (let i = 0; i < cards.length; i += 1) {
       cards[i].classList.remove("card-trip");
       cards[i].classList.add("card-trip-noclick");
     }
-  }
+  };
 
   const cardClickOff = (event) => {
-    let checkFlag = false
+    let checkFlag = false;
     for (let i = 0; i < checkBoxes.length; i += 1) {
       if (checkBoxes[i].dataset.checked === "true") {
         checkFlag = true;
@@ -103,18 +101,20 @@ if (selectTimer != null) {
         cards[i].classList.add("card-trip");
       }
     }
-  }
+  };
 
   const checkBoxClick = (event) => {
     event.preventDefault();
-    if (event.target.parentElement.dataset.checked === "false")  {// innerHTML.includes("fa-square")) {
+    if (event.target.parentElement.dataset.checked === "false") {
+      // innerHTML.includes("fa-square")) {
       event.target.parentElement.dataset.checked = "true";
       numberOfSelected += 1;
       updateSelectedCount();
       updateExportAndDeleteButton();
       showCheckCards();
       showSelectMenu();
-      event.target.parentElement.innerHTML = '<i class="far fa-check-square"></i>';
+      event.target.parentElement.innerHTML =
+        '<i class="far fa-check-square"></i>';
     } else {
       event.target.parentElement.dataset.checked = "false";
       numberOfSelected -= 1;
@@ -123,13 +123,13 @@ if (selectTimer != null) {
       hideCheckCards();
       event.target.parentElement.innerHTML = '<i class="far fa-square"></i>';
     }
-  }
+  };
 
   const cardLinkClick = (event) => {
     if (numberOfSelected > 0) {
       event.preventDefault();
     }
-  }
+  };
 
   const selectAll = (event) => {
     for (let i = 0; i < checkBoxes.length; i += 1) {
@@ -139,7 +139,7 @@ if (selectTimer != null) {
     numberOfSelected = checkBoxes.length;
     updateSelectedCount();
     updateExportAndDeleteButton();
-  }
+  };
 
   const deselectAll = (event) => {
     for (let i = 0; i < checkBoxes.length; i += 1) {
@@ -152,12 +152,12 @@ if (selectTimer != null) {
     updateSelectedCount();
     updateExportAndDeleteButton();
     hideSelectMenu();
-  }
+  };
 
   const invertSelection = (event) => {
     for (let i = 0; i < checkBoxes.length; i += 1) {
       if (checkBoxes[i].dataset.checked === "true") {
-        checkBoxes[i].dataset.checked = "false"
+        checkBoxes[i].dataset.checked = "false";
         checkBoxes[i].innerHTML = '<i class="far fa-square"></i>';
       } else {
         checkBoxes[i].dataset.checked = "true";
@@ -172,7 +172,7 @@ if (selectTimer != null) {
     hideCheckCards();
     updateSelectedCount();
     updateExportAndDeleteButton();
-  }
+  };
 
   for (let i = 0; i < checkBoxes.length; i += 1) {
     checkBoxes[i].addEventListener("click", checkBoxClick);
@@ -187,22 +187,19 @@ if (selectTimer != null) {
 
   invertSelectionButton.addEventListener("click", invertSelection);
 
-
-
-
   selectTimer.addEventListener("change", (event) => {
     Rails.ajax({
       url: "",
       type: "put",
       data: `interval=${selectTimer.value}`,
-      success: (data) => { },
-      error: (data) => {}
-    })
+      success: (data) => {},
+      error: (data) => {},
+    });
   });
 
   playButton.addEventListener("click", (event) => {
     let state = "";
-    if (playButton.innerText === "Stop")  {
+    if (playButton.innerText === "Stop") {
       state = "Stop";
     } else {
       state = "Start";
@@ -212,25 +209,27 @@ if (selectTimer != null) {
       url: "",
       type: "put",
       data: `state=${state}&interval=${selectTimer.value}`,
-      success: (data) => { },
-      error: (data) => {}
-    })
+      success: (data) => {},
+      error: (data) => {},
+    });
   });
 
   snapshotButton.addEventListener("click", (event) => {
-    snapshotButton.innerText = "Taking Snapshot..."
+    snapshotButton.innerText = "Taking Snapshot...";
     snapshotButton.disabled = true;
     Rails.ajax({
       url: "",
       type: "put",
       data: `snapshot=true`,
-      success: (data) => { },
-      error: (data) => {}
-    })
+      success: (data) => {},
+      error: (data) => {
+        console.log(data);
+      },
+    });
   });
 
   exportButton.addEventListener("click", (event) => {
-    exportButton.innerText = "Downloading..."
+    exportButton.innerText = "Downloading...";
     exportButton.disabled = true;
     deleteButton.disabled = true;
 
@@ -238,13 +237,13 @@ if (selectTimer != null) {
       url: window.location.pathname + "/createzip",
       type: "get",
       data: `files=${selectedParametersString()}`,
-      success: (data) => { },
-      error: (data) => {}
-    })
+      success: (data) => {},
+      error: (data) => {},
+    });
   });
 
   deleteButton.addEventListener("click", (event) => {
-    deleteButton.innerText = "Deleting..."
+    deleteButton.innerText = "Deleting...";
     deleteButton.disabled = true;
     exportButton.disabled = true;
 
@@ -252,9 +251,9 @@ if (selectTimer != null) {
       url: window.location.pathname + "/deletesnapshots",
       type: "get",
       data: `files=${selectedParametersString()}`,
-      success: (data) => { },
-      error: (data) => {}
-    })
+      success: (data) => {},
+      error: (data) => {},
+    });
 
     for (let i = 0; i < checkBoxes.length; i += 1) {
       if (checkBoxes[i].dataset.checked === "true") {
@@ -280,7 +279,7 @@ if (selectTimer != null) {
     hideSelectMenu();
   });
 
-  setInterval(function() {
+  setInterval(function () {
     const snapshotCards = document.querySelectorAll("#snapshot-card");
     Rails.ajax({
       url: window.location.pathname + "/refresh",
@@ -300,15 +299,7 @@ if (selectTimer != null) {
           cardLinks[i].addEventListener("click", cardLinkClick);
         }
       },
-      error: (data) => {}
-    })
+      error: (data) => {},
+    });
   }, 5000);
-
-
-
-
-
-
-
 }
-
